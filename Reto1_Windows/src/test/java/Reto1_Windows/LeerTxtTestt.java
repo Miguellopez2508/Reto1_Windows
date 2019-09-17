f13dcc06b4a0325e32be7fc6a351cc7280cdb885
IN1DM3B_01 <IN1DM3B_01@H015112.NET>
2019-09-17 09:24:13 +0200

    Clase de leer txt creada

---
+++ Reto1_Windows/src/test/java/Reto1_Windows/LeerTxtTestt.java
@@ -0,0 +1,22 @@
+package Reto1_Windows;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeerTxtTestt {
+
+	LeerTxt leerTxt = new LeerTxt();
+	
+	@Test
+	public void test() {
+		String archivoEntraStrg1 = "C:\\workspace\\Reto1_Windows\\Reto1_Windows\\archivoEntrada.txt";
+		String archivoTestStrg1 = "C:\\workspace\\Reto1_Windows\\Reto1_Windows\\archivoTest.txt";
+		String archivoEntraStrg2 = "kappa";
+		String archivoTestStrg2 = "C:\\workspace\\Reto1_Windows\\Reto1_Windows\\archivoTest2.txt";
+		
+		assertTrue(leerTxt.leerText(archivoEntraStrg1, archivoTestStrg1));
+		assertFalse(leerTxt.leerText(archivoEntraStrg2, archivoTestStrg1));
+		assertFalse(leerTxt.leerText(archivoEntraStrg1, archivoTestStrg2));
+	}
+}
